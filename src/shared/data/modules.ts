a2cdace4465a1632e2873209cac5f1bd93e77186
root <github@smallhill.cz>
2017-05-25 16:15:02 +0200

    Zmena Rozpocet a vydaje v menu na Vydaje a Rozpocet zvlast

--- src/shared/data/modules.ts
+++ src/shared/data/modules.ts
@@ -7,9 +7,10 @@ export class Module {
 
 export const MODULES: Module[] = [
 	{"id": "dash-board", "url": "prehled", "name": "Aktuálně", "optional": false},
-	{"id": "expenditure-viz", "url": "rozpocet", "name": "Rozpočet a výdaje", "optional": true},
-	{"id": "expenditure-events", "url": "investicni-akce", "name": "Investice", "optional": true},	
+	{"id": "budget-viz", "url": "rozpocet", "name": "Rozpočet", "optional": true},
+	{"id": "expenditure-viz", "url": "vydaje", "name": "Výdaje", "optional": true},
 	{"id": "income-viz", "url": "prijmy", "name": "Příjmy", "optional": true},	
+	{"id": "expenditure-events", "url": "investicni-akce", "name": "Investice", "optional": true},	
 	{"id": "notice-board", "url": "uredni-deska", "name": "Úřední deska", "optional": true},
 	{"id": "contract-list", "url": "registr-smluv", "name": "Registr smluv", "optional": true},
 	{"id": "data-catalogue", "url": "datovy-katalog", "name": "Datový katalog", "optional": true}
