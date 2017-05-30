ee65e8e436b77ca34bb8278227e70ef2eee8fe0f
root <github@smallhill.cz>
2017-05-30 13:55:30 +0200

    Upozorneni na nepodporovany prohlizec

---
+++ assets/js/compatibility.js
@@ -0,0 +1,36 @@
+function detectIE() {
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    // IE 10 or older => return version number
+    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    // IE 11 => return version number
+    var rv = ua.indexOf('rv:');
+    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    // Edge (IE 12+) => return version number
+    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+  }
+
+  // other browser
+  return false;
+}
+
+var IEVersion = detectIE();
+
+if(IEVersion && IEVersion <= 11){
+  document.body.classList.add("unsupported");
+  window.unsupportedBrowser = true;
+}
+else{
+  document.body.classList.add("supported");
+  window.unsupportedBrowser = false;
+}
