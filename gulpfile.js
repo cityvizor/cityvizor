0116ea5b6624e6e404c518834e0a1f875deff330
root <github@smallhill.cz>
2017-06-22 15:59:37 +0200

    Convert app to use Angular-Cli

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require("gulp");
-
-gulp.task("update-contracts",require("./server/tasks/update-contracts"));
-
-gulp.task("export-budgets-json",require("./server/tasks/export-budgets-json"));
-gulp.task("export-entities-json",require("./server/tasks/export-entities-json"));
-gulp.task("export-profiles-json",require("./server/tasks/export-profiles-json"));
