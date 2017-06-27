21be07a00a75931d60edcb97f6dd54a2039b703b
root <github@smallhill.cz>
2017-06-27 09:23:10 +0200

    Turn off unwanted modules

---
+++ src/config/info-texts.ts
@@ -0,0 +1,10 @@
+
+var InfoTexts = {
+  
+  "expenditures": "Výdaje jsou členěny dle účelu použití a je tedy možné vidět, za co konkrétně jsou veřejné prostředky vynakládány a komu jsou. V aplikaci Cityvizor jsou informace o skutečných výdajích spárovány s rozpočtovými daty.",
+  
+  "incomes": "Příjmy se dělí na účelové a neúčelové. Účelové příjmy jsou zpravidla vázány na konkrétní projekt (dotace národní či evropské či z jiných fondů, dary atd,). Neúčelové příjmy nejsou vázány na konkrétní projekt, zpravidla se jedná o příjmy z daní, příjmy na výkon státní správy atd."
+  
+}
+
+export { InfoTexts };
