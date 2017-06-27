69cc6994b7b5da7821f8649f1fe2f59de528ce5e
root <github@smallhill.cz>
2017-06-27 09:20:15 +0200

    Separate server config

--- server/server-dev.js
+++ server/server-dev.js
@@ -15,7 +15,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.plugin(require('mongoose-write-stream'));
 mongoose.plugin(require('mongoose-paginate'));
-mongoose.connect('mongodb://localhost/cityvizor');
+mongoose.connect('mongodb://localhost/cityvizor-dev');
 
 /* Mongo Express */
 var mongo_express = require('mongo-express/lib/middleware');
@@ -27,7 +27,6 @@ app.use('/db', mongo_express(mongo_express_config))
 app.use("/api",require("./routers/api.js"));
 
 app.use(require("./routers/static"));
-
 
 /* START SERVER */
 app.listen(8080, function () {
