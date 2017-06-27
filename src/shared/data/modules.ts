87d3c611ecb9d40ee6f64b9af034f0d63cf39cb6
root <github@smallhill.cz>
2017-06-27 09:35:39 +0200

    Turn off unwanted modules

--- src/shared/data/modules.ts
+++ src/shared/data/modules.ts
@@ -1,3 +1,10 @@
+
+/**
+	* module list has been moved to app-config.js, this file is for backwards compatibility
+	*/
+
+import { AppConfig } from "../../config/app-config";
+
 export class Module {
 	public id: string;
 	public url: string;
@@ -5,13 +12,4 @@ export class Module {
 	public optional: boolean;
 }
 
-export const MODULES: Module[] = [
-	{"id": "dash-board", "url": "prehled", "name": "Aktuálně", "optional": false},
-	{"id": "budget-viz", "url": "rozpocet", "name": "Rozpočet", "optional": true},
-	{"id": "expenditure-viz", "url": "vydaje", "name": "Výdaje", "optional": true},
-	{"id": "income-viz", "url": "prijmy", "name": "Příjmy", "optional": true},	
-	{"id": "expenditure-events", "url": "investicni-akce", "name": "Investice", "optional": true},	
-	{"id": "notice-board", "url": "uredni-deska", "name": "Úřední deska", "optional": true},
-	{"id": "contract-list", "url": "registr-smluv", "name": "Registr smluv", "optional": true},
-	{"id": "data-catalogue", "url": "datovy-katalog", "name": "Datový katalog", "optional": true}
-];
+export const MODULES:Module[] = AppConfig.modules;
