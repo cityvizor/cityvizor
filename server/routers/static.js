b8ddfd7fbbafd3d06cdd4ad8595aec300621d4e8
root <github@smallhill.cz>
2017-07-19 17:00:57 +0200

    Fix exports routing

--- server/routers/static.js
+++ server/routers/static.js
@@ -12,8 +12,8 @@ router.get("/dist", (req,res) => res.sendStatus(404));
 router.use('/assets', express.static("assets"));
 router.get("/assets", (req,res) => res.sendStatus(404));
 
-router.use('/data', express.static("exports"));
-router.get("/data", (req,res) => res.sendStatus(404));
+router.use('/exports', express.static("exports"));
+router.get("/exports", (req,res) => res.sendStatus(404));
 
 router.get("/favicon.ico", (req,res) => res.sendStatus(404));
 
