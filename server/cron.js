07504eddc97e4e1c1a949970a78a7e15c2c0abfa
root <github@smallhill.cz>
2017-08-21 15:03:51 +0200

    Prepare for PM2

--- server/cron.js
+++ server/cron.js
@@ -1,49 +1,58 @@
+var CronJob = require('cron').CronJob;
 var mongoose = require('mongoose');
 
 var config = require("./config/config");
 
 console.log("##### MIDNIGHT CRON RUN #####");
 
-try {
+var job = new CronJob({
+  cronTime: config.cron.time,//'00 00 01 * * *',
+  onTick: function() {
 
-  mongoose.Promise = global.Promise;
-  mongoose.plugin(require('mongoose-write-stream'));
-  mongoose.plugin(require('mongoose-paginate'));
-  mongoose.connect('mongodb://localhost/' + config.database.db);
-  console.log("DB connecting to database " + config.database.db);
+    try {
 
-  // set the tasks
-  var tasks = [];
+      mongoose.Promise = global.Promise;
+      mongoose.plugin(require('mongoose-write-stream'));
+      mongoose.plugin(require('mongoose-paginate'));
+      mongoose.connect('mongodb://localhost/' + config.database.db);
+      console.log("DB connecting to database " + config.database.db);
 
-  tasks.push(require("./cron/export-profiles-json"));
-  tasks.push(require("./cron/export-profiles-csv"));
-  tasks.push(require("./cron/export-budgets-json"));
-  tasks.push(require("./cron/export-budgets-csv"));
-  tasks.push(require("./cron/export-events-json"));
-  tasks.push(require("./cron/export-events-csv"));
-  tasks.push(require("./cron/download-contracts"));
+      // set the tasks
+      var tasks = [];
 
-  console.log("Starting tasks...");
-  // loop through the tasks one by one
-  runTaskLoop(tasks,() => {
+      tasks.push(require("./cron/export-profiles-json"));
+      tasks.push(require("./cron/export-profiles-csv"));
+      tasks.push(require("./cron/export-budgets-json"));
+      tasks.push(require("./cron/export-budgets-csv"));
+      tasks.push(require("./cron/export-events-json"));
+      tasks.push(require("./cron/export-events-csv"));
+      tasks.push(require("./cron/download-contracts"));
 
-    console.log("Disconnecting DB");
-    mongoose.disconnect(() => {
-      console.log("Finished!");
-      process.exit();
-    });
+      console.log("Starting tasks...");
+      // loop through the tasks one by one
+      runTaskLoop(tasks,() => {
 
-  });
-}
-catch(err) {
-  console.log(err);
-}
+        console.log("Disconnecting DB");
+        mongoose.disconnect(() => {
+          console.log("Finished!");
+        });
+
+      });
+    }
+    catch(err) {
+      console.log(err);
+    }
+  },
+  start: true, /* Start the job right now */
+  runOnInit: true,
+  timezone: 'Europe/Prague' /* Time zone of this job. */
+});
 
 function runTaskLoop(tasks,cb){
   let task = tasks.shift();
 
   if(!task) return cb();
-  
+
   console.log("===================================");
 
   task(() => {
