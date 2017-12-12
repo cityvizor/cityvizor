a6becc48fe4fbe65d8484c48401ef82cf41b79bc
Martin Kopeček <github@smallhill.cz>
2017-12-12 15:00:56 +0100

    Script to create admin user account

---
+++ server/scripts/create-admin.js
@@ -0,0 +1,35 @@
+var config = require("../config/config.js");
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/' + config.database.db, { useMongoClient: true });
+mongoose.plugin(require('mongoose-write-stream'));
+mongoose.plugin(require('mongoose-paginate'));
+mongoose.Promise = global.Promise;
+
+var User = require("../models/user");
+var bcrypt = require("bcrypt");
+
+bcrypt.hash("admin", 10)
+  .then(hash => {
+
+    var userData = {
+      "password": hash,
+      "managedProfiles": [],
+      "roles": ["admin"]
+    };
+
+    User.findOneAndUpdate({"_id": "admin"},userData,{upsert:true})
+      .then(() => {
+        console.log("User admin@example.com saved");
+        mongoose.disconnect(() => process.exit());
+      })
+      .catch(err => {
+        console.error(err.message);
+        mongoose.disconnect(() => process.exit());
+      });
+
+  })
+  .catch(err => {
+    console.error(err.message);
+    mongoose.disconnect(() => process.exit());
+  });
