e8cba03a9a97645adcf812a3b5efaa664bca0fa3
root <github@smallhill.cz>
2018-03-07 18:50:04 +0100

    Modify to go with new codelists storage

--- src/services/codelist.service.ts
+++ src/services/codelist.service.ts
@@ -4,78 +4,25 @@ import 'rxjs/add/operator/toPromise';
 
 import { Codelist } from "../shared/schema/codelist";
 
-class CodelistCache{
-	[k: string]: Codelist[];
-}
-
 @Injectable()
 export class CodelistService {
-	
-	cache:CodelistCache = new CodelistCache();
+
+	codelists:{ [key: string]: Promise<Codelist> } = {};
 	
 	constructor(private http: HttpClient) { }
 	
-	
-	// alias for getCodelist
-	get(name:string,date:Date):Promise<any>{
-			return this.getCodelist(name,date);
+	getCodelist(id:string):Promise<Codelist>{
+
+		if(!this.codelists[id]){
+			this.codelists[id] = this.http.get<Codelist>("/api/codelists/" + id).toPromise().then(codelist => new Codelist(codelist));
+		}
+		
+		return this.codelists[id];
+
 	}
 	
-	// main get method
-	getCodelist(name:string,date:Date):Promise<any>{
-		return new Promise((resolve,reject) => {
-			
-			let cached = this.getCached(name,date);
-											
-			// if we have already this codelist, then resolve to the cached value
-			if(cached) return resolve(cached);
-			
-			let dateString = date.toISOString();
-			
-			// otherwise get the codelist from the server
-			this.http.get<Codelist>("/api/codelists/" + name + "?date=" + dateString).toPromise()
-				.then(response => resolve(this.saveCached(name,response)))
-				.catch(err => reject(err));
-				
-		});
-	}
-	
-	/* Search for codelist. Return if found, otherwise return false. */
-	getCached(name:string,date:Date){
-		
-		// here we save the codelist during search
-		var codelist;
-		
-		// if no codelist of that name return false immediately
-		if(!this.cache[name]) return false;
-		
-		// search until first item found
-		var matched = this.cache[name].some(item => {
-			
-			// check date validity. if correct, assign to codelist variable and stop search by returning true. otherwise return false and continue
-			if(item.validFrom <= date && item.validTill >= date){
-				codelist = item;
-				return true;
-			}
-			else return false;
-			
-		});
-		
-		// if some codelist was matched return the codelist contents (not the metadata parent)
-		if(matched) return codelist.codelist;
-		return false;
-	}
-				 
-	saveCached(name:string,codelist:Codelist){
-		
-		// if no list was present, we have to create the array
-		if(!this.cache[name]) this.cache[name] = [];
-		
-		// we simply add the codelist to the array
-		this.cache[name].push(codelist);
-		
-		// return the codelist. the return is so that in future versions when some transformation would be present we have common interface to get the just saved codelist
-		return codelist.codelist;
+	getName(codelist:string,id:string,date:Date):Promise<string>{
+		return this.getCodelist(codelist).then(codelist => codelist ? codelist.getName(id,date) : id)
 	}
 
 }
--- src/shared/schema/codelist.ts
+++ src/shared/schema/codelist.ts
@@ -1,15 +1,48 @@
 export class Codelist {
-  _id:string;
-  name:string;
-  validFrom:Date;
-  validTill:Date;
-  codelist:any;
+  
+  public _id:string;
+  public codelist:CodelistEntry[];
+  
+  private codelistIndex:any = {};
+  
+  constructor(data?:Partial<Codelist>){
+    this._id = data._id;
+    this.codelist = data.codelist.map(item => {
+      return {
+        id: item.id,
+        name: item.name,
+        validFrom: item.validFrom ? new Date(item.validFrom) : null,
+        validTill: item.validTill ? new Date(item.validTill) : null
+      };
+    });
+  }
+  
+  checkDate(entry:CodelistEntry, date: Date){
+    date.setHours(0,0,0,0);
+    return (!entry.validFrom || entry.validFrom.getTime() <= date.getTime()) && (!entry.validTill || entry.validTill.getTime() >= date.getTime());
+  }
+  
+  getName(id:string,date:Date){
+    return this.codelistIndex[id] ? this.codelistIndex[id].filter(entry => this.checkDate(entry,date))[0] : id;
+  }
+  
+  getNames(date:Date){
+    return this.codelist.filter(entry => this.checkDate(entry,date)) || [];
+  }
+  
+  getIndex(date:Date){
+    
+    var index = {};
+    
+    this.getNames(date).forEach(entry => index[entry.id] = entry.name);
+    
+    return index;
+  }
 }
 
-export class ParagraphNamesCodelist {
-  [k: string]: string;
-}
-
-export class ItemNamesCodelist {
-  [k: string]: string;
+export interface CodelistEntry{
+  id: string;
+  name: string;
+  validFrom: Date;
+  validTill: Date;
 }
