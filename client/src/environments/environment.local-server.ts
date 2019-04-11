16b6a7d4a45f1f243a716b6ffcbee2dcba995c41
SmallhillCZ <github@smallhill.cz>
2019-04-11 10:43:05 +0200

    Fix wrong api path

--- client/src/environments/environment.local-server.ts
+++ client/src/environments/environment.local-server.ts
@@ -1,6 +1,6 @@
 export const environment = {
   name: "local-server",
   production: true,
-  api_root: "http://localhost:4300",
+  api_root: "http://localhost:4300/api",
   jwtDomains: ['localhost']
 };
