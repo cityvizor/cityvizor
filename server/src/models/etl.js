968e413c3c99c7d79d5403971d4a001ec822612f
SmallhillCZ <github@smallhill.cz>
2019-07-04 14:18:59 +0200

    Simplify ETL schema

--- server/src/models/etl.js
+++ server/src/models/etl.js
@@ -2,27 +2,18 @@ var mongoose = require('mongoose');
 
 var Profile = require("../models/profile");
 
-var etlSchema =  mongoose.Schema({
-	"profile": {type: mongoose.Schema.Types.ObjectId, ref: "Profile"},
-	"year": {type: Number, required: true},
-	"visible": Boolean,
-	
-	"enabled": Boolean,
-	"importer": String,
-	"dataFile": String,
-	"eventsFile": String,
-	"delimiter": String,
+var etlSchema = mongoose.Schema({
+	"profile": { type: mongoose.Schema.Types.ObjectId, ref: "Profile" },
+	"year": { type: Number, required: true },
+	"visible": { type: Boolean, default: false },
+
+	"validity": Date,
 
 	"status": String,
-	"error": String,
-	"validity": Date,
-	"lastCheck": Date,
-	"lastModified": Date,
-	"etag": String,
-	"warnings": [String],
+	"error": String
 
 });
-etlSchema.index({ profile: 1, year: -1 }, {unique: true});
+etlSchema.index({ profile: 1, year: -1 }, { unique: true });
 etlSchema.index({ profile: 1, visible: 1 });
 
 module.exports = mongoose.model('ETL', etlSchema);
