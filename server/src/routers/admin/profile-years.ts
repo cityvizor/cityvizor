9bbfe0e0a1b208e043875c25bedab34491ee3e14
SmallhillCZ <github@smallhill.cz>
2019-09-13 01:28:01 +0200

    Fix listing only one year due to bad sql column reference

--- server/src/routers/admin/profile-years.ts
+++ server/src/routers/admin/profile-years.ts
@@ -15,10 +15,10 @@ router.get("/", acl("profile-years", "list"), async (req, res, next) => {
 
   // select status and last time for the latest import
   const years = await db<YearRecordWithImportStatus[]>("app.years AS y")
-    .select(db.raw("distinct on (??,??) y.*, ??, GREATEST(??) as status_time", ["i.profileId", "i.year", "i.status", ["i.created", "i.started", "i.finished"]]))
+    .select(db.raw("distinct on (??) y.*, ??, GREATEST(??) as status_time", [["y.profileId", "y.year"], "i.status", ["i.created", "i.started", "i.finished"]]))
     .leftJoin("app.imports AS i", { "i.profileId": "y.profileId", "i.year": "y.year" })
     .where({ "y.profileId": req.params.profile })
-    .orderBy([{ column: "i.profileId", order: "desc" }, { column: "i.year", order: "desc" }, { column: "i.id", order: "desc" }]);
+    .orderBy([{ column: "y.profileId", order: "asc" }, { column: "y.year", order: "asc" }, { column: "i.id", order: "desc" }]);
 
   res.json(years);
 
