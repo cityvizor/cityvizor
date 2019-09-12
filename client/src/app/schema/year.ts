d04baaeeb8ec5375fcd2610bd7746e769d77fd0c
SmallhillCZ <github@smallhill.cz>
2019-09-12 18:07:59 +0200

    Show import status on years (data tab)

--- client/src/app/schema/year.ts
+++ client/src/app/schema/year.ts
@@ -2,4 +2,8 @@ export interface BudgetYear {
   profileId: number;
   year: number;
   hidden: boolean;
+  validity: string;
+
+  status?: string;
+  statusTime?: string;
 }
--- server/src/routers/admin/profile-years.ts
+++ server/src/routers/admin/profile-years.ts
@@ -1,14 +1,24 @@
 import express from "express";
 import { db } from "../../db";
 import { YearRecord } from "../../schema";
+import { ImportRecord } from "../../schema/database/import";
+
+import acl from "express-dynacl";
 
 const router = express.Router({ mergeParams: true });
 
 export const AdminProfileYearsRouter = router;
 
-router.get("/", async (req, res, next) => {
+export type YearRecordWithImportStatus = YearRecord & Pick<ImportRecord,"status"|"created">;
 
-  const years = await db<YearRecord[]>("app.years").where({ profileId: req.params.profile });
+router.get("/", acl("admin-years","list"), async (req, res, next) => {
+
+  // select status and last time for the latest import
+  const years = await db<YearRecordWithImportStatus[]>("app.years AS y")
+    .select(db.raw("distinct on (??,??) y.*, ??, GREATEST(??) as status_time", ["i.profileId", "i.year", "i.status", ["i.created", "i.started", "i.finished"]]))
+    .leftJoin("app.imports AS i", { "i.profileId": "y.profileId", "i.year": "y.year" })
+    .where({ "y.profileId": req.params.profile })
+    .orderBy([{ column: "i.profileId", order: "desc" }, { column: "i.year", order: "desc" }, { column: "i.id", order: "desc" }]);
 
   res.json(years);
 
@@ -18,12 +28,12 @@ router.get("/", async (req, res, next) => {
 router.put("/:year", async (req, res, next) => {
   var data = { profile_id: req.params.profile, year: req.params.year };
 
-  try{
+  try {
     await db("app.years").insert(data);
   }
-  catch(err) {
+  catch (err) {
     const existingYear = await db("app.years").where(data).first();
-    if(existingYear) return res.status(200).send("Year already exists.");
+    if (existingYear) return res.status(200).send("Year already exists.");
   }
 
   res.sendStatus(201);
