99c95fe9982f042d723ab09feb283f1c86ed9d71
SmallhillCZ <github@smallhill.cz>
2019-09-26 15:48:51 +0200

    Retry connection one more time when error

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,12 @@
 import Knex from 'knex';
+import Client from 'knex/lib/client';
 
 import knexConfig from "./config/knexfile";
 
-let connectionString:string;
-if(typeof knexConfig.connection === "string") connectionString = knexConfig.connection;
-else if("user" in knexConfig.connection && "database" in knexConfig.connection) connectionString = knexConfig.connection.user + "@" + knexConfig.connection.database;
-else if("filename" in knexConfig.connection) connectionString = knexConfig.connection.filename;
+let connectionString: string;
+if (typeof knexConfig.connection === "string") connectionString = knexConfig.connection;
+else if ("user" in knexConfig.connection && "database" in knexConfig.connection) connectionString = knexConfig.connection.user + "@" + knexConfig.connection.database;
+else if ("filename" in knexConfig.connection) connectionString = knexConfig.connection.filename;
 else connectionString = knexConfig.connection.user;
 
 console.log(`[DB] DB connection set to ${connectionString}`);
@@ -14,10 +15,40 @@ console.log(`[DB] DB connection set to ${connectionString}`);
 var types = require('pg').types;
 types.setTypeParser(1700, 'text', parseFloat);
 
+// hack because of postgre losing connections and Knex not being able to cope. sorry
+const oldQuery = Client.prototype.query;
+
+Client.prototype.validateConnection = function (connection) {
+  return !connection.__knex__disposed;
+};
+
+Client.prototype.query = async function (connection, obj) {
+  try {    
+    return await oldQuery.call(this, connection, obj)
+  }
+  catch (err) {
+
+    console.log("[DB] Query failed, reason: " + err.message);
+
+    // kill previous connection (when acquiring from Pool tarn.js will ask for Knex validate function and that will filter out when __knex__disposed)
+    connection.__knex__disposed = err;
+
+    console.log("[DB] Trying one more time with another connection from pool.");
+
+    // try one more time
+    const secondConnection = await this.acquireConnection().disposer(function () {
+      return this.releaseConnection(secondConnection);
+    });
+
+    return oldQuery.call(this, secondConnection, obj);
+  }
+};
+
 // Initialize knex.
 export const db = Knex(knexConfig);
 
-(async function(){
+
+(async function () {
   console.log("[DB] Running migrations");
   await db.migrate.latest(knexConfig.migrations);
   console.log("[DB] Migrations finished");
