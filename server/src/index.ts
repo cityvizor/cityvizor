76a473024d4d0b7a53c3cbe793ea4de05540161a
SmallhillCZ <github@smallhill.cz>
2019-10-09 11:56:24 +0200

    Log CityVizor version on startup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,10 @@
+const packageJSON = require("../../package.json");
 
 (async function(){
 
   console.log("Starting CityVizor");
   console.log("Node version: " + process.version);
+  console.log("CityVizor version: " + packageJSON.version);
 
   /* SERVER */
   await import("./server");
