77b00dbe681738bd7bc079dc1685ccfc5521fab0
SmallhillCZ <github@smallhill.cz>
2019-12-18 12:41:00 +0100

    Revert "Change prague map to Czech Republic"
    This reverts commit a3f42e6852616d491928a455af76a0e088af1eea.

--- client/src/app/views/frontpage/views/profile-search/profile-search.component.ts
+++ client/src/app/views/frontpage/views/profile-search/profile-search.component.ts
@@ -17,8 +17,7 @@ export class ProfileSearchComponent implements OnInit {
 	profiles: Profile[] = [];
 	hoverProfile: Profile | null;
 
-	// Prague: mapGPSBounds = { "lat": { "min": 49.9476767, "max": 50.1774944 }, "lng": { "min": 14.2244208, "max": 14.7070583 } };
-	mapGPSBounds = { "lat": { "min": 48.5525, "max": 51.0556 }, "lng": { "min": 12.0914, "max": 18.8589 } };
+	mapGPSBounds = { "lat": { "min": 49.9476767, "max": 50.1774944 }, "lng": { "min": 14.2244208, "max": 14.7070583 } };
 
 	loading: boolean = false;
 
