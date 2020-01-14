7419f8faabecfbfa658adbba3b9cd3aea43b0ead
rozklad <jan.rozklad@gmail.com>
2020-01-14 11:44:07 +0100

    93: (WIP) client + strapi cityvizor

--- client-redesign/src/main.js
+++ client-redesign/src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+Vue.mixin({
+  data() {
+    return {
+      apiBaseUrl: this.environment === 'production' ? 'https://cityvizor.cesko.digital/api/v2/service/citysearch' : 'http://localhost:1337/citysearch'
+    };
+  },
+  computed: {
+    environment() {
+      return process.env.NODE_ENV;
+    }
+  }
+});
+
 new Vue({
   router,
   render: h => h(App),
