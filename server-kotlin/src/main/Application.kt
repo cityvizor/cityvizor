7f0cd3bd1637f86b09d5920182ceb0bf9450216d
michalsvec <kontakt@michalsvec.cz>
2020-02-02 22:42:05 +0100

    Api endpoint for new city request form. closes #117 (#126)
    * api endpoint for new city request form
    
    * used correct way of handling google service account, city request extended with datetime and IP

--- server-kotlin/src/main/Application.kt
+++ server-kotlin/src/main/Application.kt
@@ -1,27 +1,28 @@
 package main
 
 import akka.actor.ActorSystem
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
-import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
+import digital.cesko.city_request.cityRequestRouter
+import digital.cesko.city_request.google_sheets.GoogleSheets
 import digital.cesko.city_search.CitySearchService
 import digital.cesko.routers.citySearchRouter
-import io.ktor.jackson.*
-import io.ktor.features.*
+import io.ktor.application.Application
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.XForwardedHeaderSupport
+import io.ktor.jackson.jackson
+import io.ktor.routing.routing
 
 object ApplicationData {
-    lateinit var system: ActorSystem
-        private set
+    private lateinit var system: ActorSystem
 
-    fun init() {
-        /*
-            Create ActorSystem
-         */
-        system = ActorSystem.create("cdbackend", ConfigFactory.load("application.conf").resolve())
+    fun init(configuration: Config) {
+
+        // Create ActorSystem
+        system = ActorSystem.create("cdbackend", configuration)
 
         CitySearchService.create(system)
     }
@@ -32,7 +33,10 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    ApplicationData.init()
+
+    val configuration = ConfigFactory.load("application.conf").resolve()
+
+    ApplicationData.init(configuration)
 
     install(Authentication) {
     }
@@ -43,8 +47,19 @@ fun Application.module(testing: Boolean = false) {
         }
     }
 
+    install(XForwardedHeaderSupport)    // city request logs remote IP
+
     routing {
         citySearchRouter()
+        cityRequestRouter(
+            configuration.getString("app.city-request.timeZone"),
+            GoogleSheets(
+                System.getProperty("googleCredentials"),
+                configuration.getString("app.city-request.sheetId"),
+                configuration.getString("app.city-request.listName"),
+                configuration.getString("app.city-request.appName")
+            )
+        )
     }
 }
 
