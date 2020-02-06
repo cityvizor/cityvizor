29c643925f2911363a6e4d16dcdc1b9c09c7592d
Honza <jan.vodera@seznam.cz>
2020-02-06 19:22:38 +0100

    return state of city sync changed to 201 created

--- server-kotlin/src/city_sync/CitySynchronizationRouter.kt
+++ server-kotlin/src/city_sync/CitySynchronizationRouter.kt
@@ -1,16 +1,18 @@
 package digital.cesko.city_sync
 
-import city_sync.CitySynchronizationService
-import city_sync.model.SyncTask
 import city_sync.exception.CitySyncException
+import digital.cesko.city_sync.model.SyncTask
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.*
+import io.ktor.util.KtorExperimentalAPI
 import main.kodein
+import org.apache.http.HttpStatus
 import org.kodein.di.generic.instance
 
+@KtorExperimentalAPI
 fun Routing.citySynchronizationRouter(): Route {
     val citySyncService by kodein.instance<CitySynchronizationService>()
 
@@ -28,10 +30,10 @@ fun Routing.citySynchronizationRouter(): Route {
         }
         post("/synchronization") {
             val body = call.receive<SyncTask>()
-            val result =
-                citySyncService.syncCityDetails(body)
 
-            call.respond(result)
+            citySyncService.syncCityDetails(body)
+
+            call.respond(HttpStatus.SC_CREATED)
         }
     }
 }
