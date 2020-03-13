c9b0e3263344c8d8420ee96fd1515b142fc00e71
SmallhillCZ <github@smallhill.cz>
2020-03-13 13:38:31 +0100

    Remove wuery retri hack

--- server/src/db.ts
+++ server/src/db.ts
@@ -16,33 +16,33 @@ var types = require('pg').types;
 types.setTypeParser(1700, 'text', parseFloat);
 
 // hack because of postgre losing connections and Knex not being able to cope. sorry
-const oldQuery = Client.prototype.query;
+// const oldQuery = Client.prototype.query;
 
-Client.prototype.validateConnection = function (connection) {
-  return !connection.__knex__disposed;
-};
+// Client.prototype.validateConnection = function (connection) {
+//   return !connection.__knex__disposed;
+// };
 
-Client.prototype.query = async function (connection, obj) {
-  try {    
-    return await oldQuery.call(this, connection, obj)
-  }
-  catch (err) {
+// Client.prototype.query = async function (connection, obj) {
+//   try {    
+//     return await oldQuery.call(this, connection, obj)
+//   }
+//   catch (err) {
 
-    console.log("[DB] Query failed, reason: " + err.message);
+//     console.log("[DB] Query failed, reason: " + err.message);
 
-    // kill previous connection (when acquiring from Pool tarn.js will ask for Knex validate function and that will filter out when __knex__disposed)
-    connection.__knex__disposed = err;
+//     // kill previous connection (when acquiring from Pool tarn.js will ask for Knex validate function and that will filter out when __knex__disposed)
+//     connection.__knex__disposed = err;
 
-    console.log("[DB] Trying one more time with another connection from pool.");
+//     console.log("[DB] Trying one more time with another connection from pool.");
 
-    // try one more time
-    const secondConnection = await this.acquireConnection().disposer(function () {
-      return this.releaseConnection(secondConnection);
-    });
+//     // try one more time
+//     const secondConnection = await this.acquireConnection().disposer(function () {
+//       return this.releaseConnection(secondConnection);
+//     });
 
-    return oldQuery.call(this, secondConnection, obj);
-  }
-};
+//     return oldQuery.call(this, secondConnection, obj);
+//   }
+// };
 
 // Initialize knex.
 export const db = Knex(knexConfig);
