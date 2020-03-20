002ed627af8878ba22aeaf0bafa7f44fd86eff08
lukas-krecan <lukas@krecan.net>
2020-03-20 20:30:09 +0100

    Replace PathParam by RequestParam

--- server-kotlin/src/main/kotlin/digital/cesko/city_search/CitySearchController.kt
+++ server-kotlin/src/main/kotlin/digital/cesko/city_search/CitySearchController.kt
@@ -3,13 +3,13 @@ package digital.cesko.city_search
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
 
 @RestController()
 class CitySearchController {
     @GetMapping("/api/v2/service/citysearch")
-    fun search(@PathParam("query") query: String?) = CitySearchService.search(CitySearchIndex.Search(query ?: ""))
+    fun search(@RequestParam("query") query: String?) = CitySearchService.search(CitySearchIndex.Search(query ?: ""))
 
     @PostMapping("/api/v2/service/citysearch/update")
     fun update(): String {
