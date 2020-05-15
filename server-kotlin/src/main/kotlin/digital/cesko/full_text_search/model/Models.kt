3f395bda5cf17dd972ab6cd548017163cb376151
Michal Svec <kontakt@michalsvec.cz>
2020-05-15 22:22:42 +0200

    non blocking index refresh and better analyzer allowing multi words searches

--- server-kotlin/src/main/kotlin/digital/cesko/full_text_search/model/Models.kt
+++ server-kotlin/src/main/kotlin/digital/cesko/full_text_search/model/Models.kt
@@ -1,5 +1,8 @@
 package digital.cesko.full_text_search.model
 
+import digital.cesko.full_text_search.service.*
+import org.apache.lucene.document.Document
+
 data class SearchResult(
         val id: String,
         val profile: String,
@@ -9,4 +12,15 @@ data class SearchResult(
         val amount: Long?,
         val currency: String?,
         val url: String?
-)
+) {
+    constructor(doc: Document) : this(
+            doc.get(CONTRACT_ID),
+            doc.get(CONTRACT_PROFILE),
+            doc.get(CONTRACT_DATE),
+            doc.get(CONTRACT_TITLE),
+            doc.get(CONTRACT_COUNTERPARTY),
+            doc.get(CONTRACT_AMOUNT)?.toLong(),
+            doc.get(CONTRACT_CURRENCY),
+            doc.get(CONTRACT_URL)
+    )
+}
--- server-kotlin/src/main/kotlin/digital/cesko/full_text_search/service/SearchService.kt
+++ server-kotlin/src/main/kotlin/digital/cesko/full_text_search/service/SearchService.kt
@@ -1,18 +1,14 @@
 package digital.cesko.full_text_search.service
 
+import digital.cesko.common.AccentInsensitiveAnalyzer
 import digital.cesko.full_text_search.model.SearchResult
 import org.apache.lucene.document.IntPoint
 import org.apache.lucene.index.DirectoryReader
-import org.apache.lucene.index.Term
-import org.apache.lucene.search.BooleanClause
-import org.apache.lucene.search.BooleanQuery
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser
 import org.apache.lucene.search.IndexSearcher
-import org.apache.lucene.search.WildcardQuery
 import org.apache.lucene.store.Directory
-import org.springframework.beans.factory.config.ConfigurableBeanFactory
-import org.springframework.context.annotation.Scope
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
-
 
 const val CONTRACT_TITLE = "title"
 const val CONTRACT_COUNTERPARTY = "counterparty"
@@ -25,40 +21,32 @@ const val CONTRACT_ID = "id"
 const val MAX_SIZE = 30
 
 @Service
-@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
 class SearchService(
-        private var directory: Directory
+        @Qualifier("fulltextIndex") private val directory: Directory
 ) {
+    private final val analyzer = AccentInsensitiveAnalyzer()
+    private final val queryParser = MultiFieldQueryParser(arrayOf(CONTRACT_COUNTERPARTY, CONTRACT_TITLE), analyzer)
+
     fun countById(contractId: Int): Int {
         val searcher = IndexSearcher(DirectoryReader.open(directory))
         return searcher.count(IntPoint.newExactQuery(CONTRACT_ID, contractId))
     }
 
     fun search(stringQuery: String, profile: String?): List<SearchResult> {
-        val contractCounterParty = WildcardQuery(Term(CONTRACT_COUNTERPARTY, "$stringQuery*".toLowerCase()))
-        val contractTitle = WildcardQuery(Term(CONTRACT_TITLE, "$stringQuery*".toLowerCase()))
+        DirectoryReader.open(directory).use { reader ->
+            val searcher = IndexSearcher(reader)
 
-        val chainQueryBuilderForFulltext = BooleanQuery.Builder()
-        chainQueryBuilderForFulltext.add(contractCounterParty, BooleanClause.Occur.SHOULD)
-        chainQueryBuilderForFulltext.add(contractTitle, BooleanClause.Occur.SHOULD)
+            val split = stringQuery.split(" ").filter { it.isNotEmpty() }
+            var hits = searcher.search(queryParser.parse("${split.joinToString(" AND ")}*"), MAX_SIZE)
 
-        val finalQuery = chainQueryBuilderForFulltext.build()
-        val searcher = IndexSearcher(DirectoryReader.open(directory))
-        val topDocs = searcher.search(finalQuery, MAX_SIZE)
-        val mapped = topDocs.scoreDocs.map {
-            val doc = searcher.doc(it.doc)
-            val result = SearchResult(
-                    id = doc.get(CONTRACT_ID),
-                    profile = doc.get(CONTRACT_PROFILE),
-                    title = doc.get(CONTRACT_TITLE),
-                    counterparty = doc.get(CONTRACT_COUNTERPARTY),
-                    amount = doc.get(CONTRACT_AMOUNT)?.toLong(),
-                    date = doc.get(CONTRACT_DATE),
-                    currency = doc.get(CONTRACT_CURRENCY),
-                    url = doc.get(CONTRACT_URL)
-            )
-            result
+            if (hits.totalHits.value == 0L) {
+                hits = searcher.search(queryParser.parse(split.map { "${it}~" }.joinToString(" AND ")), MAX_SIZE)
+            }
+
+            val mapped = hits.scoreDocs.map {
+                SearchResult(searcher.doc(it.doc))
+            }
+            return if (profile == null) mapped else mapped.filter { it.profile == profile }
         }
-        return if (profile == null) mapped else mapped.filter { it.profile == profile }
     }
 }
