a617d892d21b9e974fd7f573d803e83c675857e9
Petr Ilnytsky <petrilnytsky@gmail.com>
2020-07-16 01:48:55 +0200

    feat: add temporary `close` icon to the modal

---
+++ redesign/client-redesign/src/components/icons/Close.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- TODO: Use proper icon -->
+  <div class="close">
+    <svg :height="size" :width="size">
+      <line x1="2" y1="2" :x2="diff" :y2="diff" :style="styleObj" />
+      <line :x1="diff" y1="2" x2="2" :y2="diff" :style="styleObj" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Close',
+  props: {
+    size: {
+      type: Number,
+      default: 12
+    },
+    thickness: {
+      type: Number,
+      default: 2
+    }
+  },
+  computed: {
+    diff() {
+      return this.size - 2;
+    },
+    styleObj() {
+      return `stroke:#000;stroke-width:${this.thickness}`
+    }
+  },
+}
+</script>
+
+<style>
+</style>
