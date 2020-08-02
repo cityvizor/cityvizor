65acf9db15e6bea5daed939162991b0755e45648
Petr Ilnytsky <petrilnytsky@gmail.com>
2020-08-02 23:14:05 +0200

    feat: add simple function for generating unique IDs
    - Vue requires unique identifiers for the :key attribute so when the view has to be updated, there's no confusion on which element is affected
    - I chose simple solution that will suffice for now - https://stackoverflow.com/questions/105034/how-to-create-guid-uuid/13403498#13403498
    - Ideally we'd use something like - https://www.npmjs.com/package/vue-uuid - but I didn't want to pollute the solution with 3rd party dependencies (yet)

---
+++ redesign/client-redesign/src/js/get-guid.js
@@ -0,0 +1,8 @@
+export default function () {
+  return getRandomString() + getRandomString()
+}
+
+function getRandomString () {
+  const radix = 36
+  return Math.random().toString(radix).substring(2, 15)
+}
