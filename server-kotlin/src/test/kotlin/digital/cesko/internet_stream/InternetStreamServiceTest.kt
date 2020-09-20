06400f7bf39809b8e5c5dc1c4be0a2bb2be46976
David Bulant <david.developer.play@gmail.com>
2020-09-20 22:12:19 +0200

    Add test for service which downloads data in .csv format

--- server-kotlin/src/test/kotlin/digital/cesko/internet_stream/InternetStreamServiceTest.kt
+++ server-kotlin/src/test/kotlin/digital/cesko/internet_stream/InternetStreamServiceTest.kt
@@ -2,29 +2,42 @@ package digital.cesko.internet_stream
 
 import digital.cesko.AbstractSpringDatabaseTest
 import digital.cesko.common.Payments
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import java.math.BigDecimal
 import kotlin.test.assertEquals
 
 class InternetStreamServiceTest : AbstractSpringDatabaseTest() {
+    init {
+        // This has to be present in order to recognize classpath protocol
+        TomcatURLStreamHandlerFactory.getInstance()
+    }
+
     @Autowired
     private lateinit var internetStreamService: InternetStreamService
 
     @Test
     fun fetchBudgets() {
         internetStreamService.fetchData()
-        transaction {
-            val result = Payments.select { Payments.year eq 2012 }.first()
-
-            assertEquals(20, result[Payments.profileId])
-            assertEquals(2012, result[Payments.year])
-            assertEquals(2310, result[Payments.paragraph])
-            assertEquals(5329, result[Payments.item])
-            assertEquals(100, result[Payments.event])
-            assertEquals(BigDecimal("1036700.00"), result[Payments.amount])
+        val result: ResultRow = transaction {
+            Payments.select { Payments.year eq 2012 }.first()
         }
+
+        val map = result.toString()
+                .split(",")
+                .map { it.trim() }
+                .map { it.split("=") }
+                .map { it.first() to it.last() }
+                .toMap()
+
+        assertEquals(20, map["digital.cesko.common.Payments.profile_id"]?.toInt())
+        assertEquals(2012, map["digital.cesko.common.Payments.year"]?.toInt())
+        assertEquals(2310, map["digital.cesko.common.Payments.paragraph"]?.toInt())
+        assertEquals(5329, map["digital.cesko.common.Payments.item"]?.toInt())
+        assertEquals(100, map["digital.cesko.common.Payments.event"]?.toInt())
+        assertEquals("1036700.00", map["digital.cesko.common.Payments.amount"])
     }
 }
