8bfb9b700bf0b914f583e922cd6944ace2317c7a
Peter Tylka <peterTylka@users.noreply.github.com>
2020-12-12 00:52:29 +0100

    281: 404 page according to brand manual

--- client/src/app/views/not-found-page/not-found-page.component.ts
+++ client/src/app/views/not-found-page/not-found-page.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'not-found-page',
-    templateUrl: 'not-found-page.component.html'
+    templateUrl: 'not-found-page.component.html',
+    styleUrls: ['./not-found-page.component.scss']
 })
 
 export class NotFoundPageComponent {
