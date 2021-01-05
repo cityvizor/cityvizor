b990f843c29f88b99d62178864ac561ce61689c6
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-01-05 15:36:18 +0100

    Run GH Actions on every commit in PR. Fix build. (#301)
    * Run GH Actions on every commit in PR. Fix build.
    
    * Fix build one more time.

--- landing-page/src/router/index.js
+++ landing-page/src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router';
 import WhyPage from '../components/pages/WhyPage'
 import MarkdownPage from '../components/pages/MarkdownPage'
 import Home from '../components/pages/Home';
-import { publicPath } from "../../vue.config"
 
 Vue.use(Router);
 
@@ -55,5 +54,5 @@ const routes = [
 export default new Router({
     mode: 'history',
     routes,
-    base: publicPath
+    base: process.env.VUE_PUBLIC_PATH || "/landing"
 })
