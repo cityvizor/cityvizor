f7338c7194fa86a0a6b0be85ed637be62a3b28b4
Petr Ilnytsky <petrilnytsky@gmail.com>
2021-01-18 12:57:49 +0100

    feat: add `give feedback` form
    + update `ParticipateBox` with more reusable solution

--- landing-page/src/components/partials/FeedbackBox.vue
+++ landing-page/src/components/partials/FeedbackBox.vue
@@ -1,55 +1,30 @@
 <template>
-<b-container id="feedback-box">
-    <b-row class="feedback-box-row">
-        <h3>Máte nápady a připomínky k aplikaci Cityvizor?</h3>
-    </b-row>
-    <b-row class="feedback-box-row">
-        <div>Budeme rádi, když se o ně s námi podělíte.</div>
-    </b-row>
-    <b-row class="feedback-box-row">
-        <b-button id="feedback-modal-button" @click="open()">Chci dát zpětnou vazbu</b-button>
-    </b-row>
-</b-container>
+    <div class="feedback-box">
+        <ModalBox
+            header="Máte nápady a připomínky k aplikaci Cityvizor?"
+            subHeader="Budeme rádi, když se o ně s námi podělíte."
+            submitButtonLabel="Chci dát zpětnou vazbu"
+            @clicked="modalOpen = true" />
+        <ModalFeedback
+            v-if="modalOpen === true"
+            @close="modalOpen = false" />
+    </div>
 </template>
 
 <script>
+import ModalBox from "./ModalBox.vue"
+import ModalFeedback from "./ModalFeedback.vue"
+
 export default {
     name: "FeedbackBox",
     components: {
+        ModalBox,
+        ModalFeedback
     },
     data() {
         return {
             modalOpen: false
         }
-    },
-    methods: {
-        open() {
-            // TODO: Open the feedback form. Not implemented yet.
-        }
     }
 }
 </script>
-
-<style scoped lang="scss">
-@import '../../assets/styles/common/_variables.scss';
-
-#feedback-box {
-	border-radius: 15px;
-	border: 2px solid $text-gray;
-    margin-bottom: 10px;
-}
-
-#feedback-modal-button {
-    color: $black;
-    border: 2px solid $black;
-    font-size: 20px;
-}
-
-.feedback-box-row {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 10px;
-}
-
-</style>
--- landing-page/src/components/partials/ParticipateBox.vue
+++ landing-page/src/components/partials/ParticipateBox.vue
@@ -1,62 +1,30 @@
 <template>
-<b-container id="participate-box">
-    <b-row class="participate-box-row">
-        <h3>Chcete Cityvizor i ve vaší obci?</h3>
-    </b-row>
-    <b-row class="participate-box-row">
-        <div>Dejte nám o tom vědět pomocí jednoduchého formuláře.</div>
-    </b-row>
-    <b-row class="participate-box-row">
-        <b-button id="participate-modal-button" @click="open()">Chci zapojit svou obec</b-button>
-    </b-row>
-    <ModalRequestCity
-      v-if="modalOpen === true"
-      @close="modalOpen = false" />
-</b-container>
+    <div class="participate-box">
+        <ModalBox
+            header="Chcete Cityvizor i ve vaší obci?"
+            subHeader="Dejte nám o tom vědět pomocí jednoduchého formuláře."
+            submitButtonLabel="Chci zapojit svou obec"
+            @clicked="modalOpen = true" />
+        <ModalRequestCity
+            v-if="modalOpen === true"
+            @close="modalOpen = false" />
+    </div>
 </template>
 
 <script>
+import ModalBox from "./ModalBox.vue"
 import ModalRequestCity from "./ModalRequestCity.vue"
 
 export default {
     name: "ParticipateBox",
     components: {
+        ModalBox,
         ModalRequestCity
     },
     data() {
         return {
             modalOpen: false
         }
-    },
-    methods: {
-        open() {
-            // TODO: Open the modal. Right now, it requires a city field to open.
-            // The city field will be supplied by the user in a new version of the modal.
-        }
     }
 }
 </script>
-
-<style scoped lang="scss">
-@import '../../assets/styles/common/_variables.scss';
-
-#participate-box {
-	border-radius: 15px;
-	border: 2px solid $text-gray;
-    margin-bottom: 10px;
-}
-
-#participate-modal-button {
-    color: $black;
-    border: 2px solid $black;
-    font-size: 20px;
-}
-
-.participate-box-row {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 10px;
-}
-
-</style>
