da8a13f9bc46cdceef8e6b1fc93491c310ab408b
Richard Marko <srk@48.io>
2021-01-20 10:37:45 +0100

    nix: pin nixpkgs
    This ensure reproducibility and doesn't grab
    nixpkgs input from environment which might differ on
    developers machines.

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
-{ pkgs ? import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; } }:
+{ pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/a141de9f3a0d3c22f4dd6c8f1b8e7b90c245ea7a.tar.gz";
+    sha256 = "0dcd4z36xy82kdm381vgxfpw33dp8wpd0464mbg7s8krns607ij1";
+  })
+  { overlays = [ (import ./overlay.nix) ]; }
+}:
 
 {
   inherit (pkgs.cityvizor)
