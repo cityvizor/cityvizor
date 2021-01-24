1ecc9679421ce37e3486d3b7058288f7bc74562a
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-01-24 13:07:42 +0100

    Fix landing page's content configuration

--- landing-page/src/main.js
+++ landing-page/src/main.js
@@ -2,27 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import AsyncComputed from 'vue-async-computed'
-import alternativePageContent from "../cfg/content"
+import axios from "axios"
 
 Vue.config.productionTip = false
 Vue.use(AsyncComputed)
 
-Vue.mixin({
-  data() {
-    return {
-      // Citysearch API
-      apiBaseUrl: process.env.VUE_APP_API_BASE_URL,
-      alternativePageContent:  alternativePageContent
-    }
-  },
-  computed: {
-    environment() {
-      return process.env.NODE_ENV;
-    }
-  }
-});
+axios.get(`${process.env.BASE_URL}/cfg/content.json`)
+    .then((response) => {
+      Vue.mixin({
+        data() {
+          return {
+            // Citysearch API
+            apiBaseUrl: process.env.VUE_APP_API_BASE_URL,
+            alternativePageContent: response.data
+          }
+        },
+        computed: {
+          environment() {
+            return process.env.NODE_ENV;
+          }
+        }
+      });
+      
+      new Vue({
+        router,
+        render: h => h(App),
+      }).$mount('#app')
+  })
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
