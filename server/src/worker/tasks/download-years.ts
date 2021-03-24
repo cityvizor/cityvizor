12722d8c6a1261be0740d980c5b88f10a2768d96
Vojtěch Srdečný <vojtech.srdecny@gmail.com>
2021-03-24 14:34:52 +0100

    fix: Handle downloading data errors gracefully
    During the automated data downloading from IS (and other sources), an error (such as 404) prevents other data to be downloaded. Each year has now a chance to be downloaded, and logging is made more clear.

--- server/src/worker/tasks/download-years.ts
+++ server/src/worker/tasks/download-years.ts
@@ -15,42 +15,47 @@ export const TaskDownloadYears: CronTask = {
   exec: async () => {
     const years = await db<YearRecord>('app.years').whereNotNull('importUrl');
     for (const year of years) {
-      if (!year.importPeriodMinutes || !year.importUrl) continue;
-      const lastImport = await db<ImportRecord>('app.imports')
-        .first()
-        .where('profileId', '=', year.profileId)
-        .where('year', '=', year.year)
-        .orderBy('created', 'desc');
-      if (
-        !lastImport ||
-        lastImport.created <
-          new Date(Date.now() - 1000 * 60 * year.importPeriodMinutes)
-      ) {
-        const importDir = await Import.createImportDir();
-        await axios
-          .get(year.importUrl, {responseType: 'stream'})
-          .then(async r => {
-            const dataPath = path.join(importDir, 'data.zip');
-            const dataFileStream = fs.createWriteStream(dataPath);
-            r.data.pipe(dataFileStream);
-            dataFileStream.on('finish', async () => {
-              // TODO: Assuming each imported year has to be unzipped for now
-              await extract(dataPath, {dir: importDir});
-              dataFileStream.close();
+      try {
+        if (!year.importPeriodMinutes || !year.importUrl) continue;
+        const lastImport = await db<ImportRecord>('app.imports')
+          .first()
+          .where('profileId', '=', year.profileId)
+          .where('year', '=', year.year)
+          .orderBy('created', 'desc');
+        if (
+          !lastImport ||
+          lastImport.created <
+            new Date(Date.now() - 1000 * 60 * year.importPeriodMinutes)
+        ) {
+          const importDir = await Import.createImportDir();
+          await axios
+            .get(year.importUrl, {responseType: 'stream'})
+            .then(async r => {
+              const dataPath = path.join(importDir, 'data.zip');
+              const dataFileStream = fs.createWriteStream(dataPath);
+              r.data.pipe(dataFileStream);
+              dataFileStream.on('finish', async () => {
+                // TODO: Assuming each imported year has to be unzipped for now
+                await extract(dataPath, {dir: importDir});
+                dataFileStream.close();
+              });
             });
-          });
-        const importData: Partial<ImportRecord> = {
-          profileId: year.profileId,
-          year: year.year,
+          const importData: Partial<ImportRecord> = {
+            profileId: year.profileId,
+            year: year.year,
 
-          created: DateTime.local().toJSDate(),
-          status: 'pending',
-          error: undefined,
-          append: false,
-          importDir,
-          format: year.importFormat,
-        };
-        await db<ImportRecord>('app.imports').insert(importData);
+            created: DateTime.local().toJSDate(),
+            status: 'pending',
+            error: undefined,
+            append: false,
+            importDir,
+            format: year.importFormat,
+          };
+          await db<ImportRecord>('app.imports').insert(importData);
+          console.log(`Downloaded ${year.importUrl}`);
+        }
+      } catch (err) {
+        console.error(`Downloading ${year.importUrl} failed: ${err.message}`);
       }
     }
   },
