64f9339333b6a715ee06cca4cf87c27cf3389431
Ondřej Roztočil <roztocil@outlook.com>
2022-05-25 17:58:35 +0200

    #471 Change yearly accounting sum to filter out "hidden" items

--- server/src/routers/public/profile-years.ts
+++ server/src/routers/public/profile-years.ts
@@ -7,19 +7,43 @@ const router = express.Router({mergeParams: true});
 
 export const ProfileYearsRouter = router;
 
-router.get('/', async (req, res) => {
-  const years = await db('years as y')
+const getYearQuery = (profileId: string) =>
+  db('years as y')
     .select('y.profileId', 'y.year', 'y.validity')
-    .sum('a.expenditureAmount as expenditureAmount')
-    .sum('a.budgetExpenditureAmount as budgetExpenditureAmount')
-    .sum('a.incomeAmount as incomeAmount')
-    .sum('a.budgetIncomeAmount as budgetIncomeAmount')
     .leftJoin('accounting as a', {
       'a.profileId': 'y.profileId',
       'a.year': 'y.year',
     })
-    .where({'y.profile_id': req.params.profile})
-    .groupBy('y.profileId', 'y.year', 'y.validity');
+    .where({'y.profile_id': profileId})
+    .groupBy('y.profileId', 'y.year', 'y.validity')
+    .orderBy('y.year');
+
+router.get('/', async (req, res) => {
+  const budgetAmounts = await getYearQuery(req.params.profile)
+    .sum('a.budgetExpenditureAmount as budgetExpenditureAmount')
+    .sum('a.budgetIncomeAmount as budgetIncomeAmount');
+
+  const incomeAmounts = await getYearQuery(req.params.profile)
+    .sum('a.incomeAmount as incomeAmount')
+    .innerJoin('codelists as c', {
+      'c.id': db.raw('SUBSTRING(a.item::varchar, 1, 2)'),
+    })
+    .where({'c.codelist': 'item-groups'});
+
+  const expenditureAmounts = await getYearQuery(req.params.profile)
+    .sum('a.expenditureAmount as expenditureAmount')
+    .innerJoin('codelists as c', {
+      'c.id': db.raw('SUBSTRING(a.paragraph::varchar, 1, 2)'),
+    })
+    .where({'c.codelist': 'paragraph-groups'});
+
+  const years = budgetAmounts.map((year, index) => {
+    return {
+      ...year,
+      incomeAmount: incomeAmounts[index]?.incomeAmount ?? 0,
+      expenditureAmount: expenditureAmounts[index]?.expenditureAmount ?? 0,
+    };
+  });
 
   res.json(years);
 });
