300a98121c004f7cf3a52f78b623a5aa8f0a5f8c
oroztocil <79744616+oroztocil@users.noreply.github.com>
2022-06-11 11:07:05 +0200

    #481 Add profile switch determining which items are counted for yearly totals

---
+++ server/migrations/20220610222427_add_profile_sum_mode.js
@@ -0,0 +1,56 @@
+exports.up = function (knex) {
+  return knex.schema
+    .alterTable('app.profiles', table => {
+      table.string('sum_mode', 20).notNullable().default('complete');
+    })
+    .then(() => {
+      return knex.raw(`
+      CREATE OR REPLACE VIEW public.profiles AS SELECT 
+        profiles.id,
+        profiles.status,
+        profiles.url,
+        profiles.name,
+        profiles.email,
+        profiles.avatar_type,
+        profiles.ico,
+        profiles.databox,
+        profiles.edesky,
+        profiles.mapasamospravy,
+        profiles.gps_x,
+        profiles.gps_y,
+        profiles.main,
+        profiles.type,
+        profiles.parent,
+        profiles.popup_name,
+        profiles.sum_mode
+      FROM app.profiles`);
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .alterTable('app.profiles', table => {
+      table.dropColumn('sum_mode');
+    })
+    .then(() => {
+      return knex.raw(`
+      CREATE OR REPLACE VIEW public.profiles AS SELECT 
+        profiles.id,
+        profiles.status,
+        profiles.url,
+        profiles.name,
+        profiles.email,
+        profiles.avatar_type,
+        profiles.ico,
+        profiles.databox,
+        profiles.edesky,
+        profiles.mapasamospravy,
+        profiles.gps_x,
+        profiles.gps_y,
+        profiles.main,
+        profiles.type,
+        profiles.parent,
+        profiles.popup_name
+      FROM app.profiles`);
+    });
+};
