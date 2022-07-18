317b6f9cb21f48d2896afdc845e9305f76d2cd52
Ondřej Roztočil <roztocil@outlook.com>
2022-07-18 21:25:26 +0200

    #493 Readd missing paragraph group 62

---
+++ server/migrations/20220718205500_readd_paragraph_group_62.js
@@ -0,0 +1,15 @@
+exports.up = async function (knex) {
+  return knex('data.codelists').insert({
+    codelist: 'paragraph-groups',
+    id: '62',
+    name: 'Jiné veřejné služby a činnosti',
+    validFrom: '1990-01-01T00:00:00.000Z',
+  });
+};
+
+exports.down = async function (knex) {
+  return knex('data.codelists')
+    .where('codelist', 'paragraph-groups')
+    .andWhere('id', '62')
+    .delete();
+};
