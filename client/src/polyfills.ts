649b5a2fbc7ee93a5f8acea405e2ed2eb79465ef
Ondřej Roztočil <roztocil@outlook.com>
2022-11-27 23:54:05 +0100

    #506 Fix missing localize package

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -1,3 +1,8 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '../node_modules/@angular/localize/init'
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
