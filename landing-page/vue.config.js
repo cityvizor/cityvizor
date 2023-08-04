9d505095457aaecdf2344c5c41e5901d69cf5a00
Ondrej Roztocil <roztocil@outlook.com>
2023-08-04 18:03:57 +0200

    Update Vue CLI, fix configuration

--- landing-page/vue.config.js
+++ landing-page/vue.config.js
@@ -6,14 +6,23 @@ module.exports = {
     lintOnSave: false,
     devServer: {
         // Required for hot reload with docker container on Windows 10
-        watchOptions: {
-            ignored: /node_modules/,
-            poll: 1000
+        watchFiles: {
+            options: {
+                ignored: /node_modules/,
+                poll: 2000
+            }
         },
+        // watchOptions: {
+        //     ignored: /node_modules/,
+        //     poll: 1000
+        // },
         port: 80,
         host: '0.0.0.0',
-        public: 'localhost:4200',
-        disableHostCheck: true,
+        // public: 'localhost:4200',
+        client: {
+            webSocketURL: "auto://localhost:4200/ws"
+        },
+        allowedHosts: "all",
         // https://cli.vuejs.org/config/#devserver-proxy
         proxy: {
             '^/api': {
