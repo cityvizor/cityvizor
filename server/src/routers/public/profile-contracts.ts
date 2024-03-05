14ff6dcdf8e1e72581768efceb996e17d5ef916c
oroztocil <roztocil@outlook.com>
2024-03-05 11:10:30 +0100

    Bugfix/630 wrong limit param handling (#631)
    * #630 Handle limit query param properly
    
    * Fix eslint 'fix' npm command

--- server/src/routers/public/profile-contracts.ts
+++ server/src/routers/public/profile-contracts.ts
@@ -2,17 +2,18 @@ import express, { Request } from "express";
 
 import { db, sort2order } from "../../db";
 import { ContractRecord } from "../../schema";
+import { parseAndLimitNumber } from "../../utils";
 
 const router = express.Router({ mergeParams: true });
 
 export const ProfileContractsRouter = router;
 
 router.get("/", async (req: Request<{ profile: string }>, res) => {
-  const pLimit = Number(req.query.limit);
+  const limit = parseAndLimitNumber(req.query.limit, 100);
 
   const contracts = await db<ContractRecord>("contracts")
     .where("profile_id", req.params.profile)
-    .limit(req.query.limit ? Math.min(pLimit, 100) : pLimit)
+    .limit(limit)
     .offset(Number(req.query.offset || 0))
     .modify(function () {
       if (req.query.sort) {
