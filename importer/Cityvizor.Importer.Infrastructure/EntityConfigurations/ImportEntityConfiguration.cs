c657ea5d94def653438f56ec3fcaf43bb3bf1148
prusovak2 <54283724+prusovak2@users.noreply.github.com>
2024-03-29 14:59:20 +0100

    Feature/importer import dir (#643)
    * change configuration of status field to posgres enum
    
    * fix import dir update
    
    * ImportFormat snakecase conversion

--- importer/Cityvizor.Importer.Infrastructure/EntityConfigurations/ImportEntityConfiguration.cs
+++ importer/Cityvizor.Importer.Infrastructure/EntityConfigurations/ImportEntityConfiguration.cs
@@ -12,17 +12,44 @@ internal class ImportEntityConfiguration : IEntityTypeConfiguration<Import>
 
         builder.HasKey(x => x.Id);
 
-        builder.Property(i => i.Status)
-            .HasConversion(
-                v => v.ToString(),
-                v =>Enum.Parse<ImportStatus>(v, true));
+        // status enum is enum even in posgress and needs to be handled in a special way in dbContext OnModelCreating and when registering db context 
 
+        // manual snake_case conversion
         builder.Property(i => i.Format)
             .HasConversion(
-                v => v.ToString(),
-                v => Enum.Parse<ImportFormat>(v, true));
+                enumValue => ImportFormatToStringConversion(enumValue),
+                stringValue => StringToImportFormatConversion(stringValue));
 
         builder.Property(i => i.ImportDir)
             .HasMaxLength(255);
+    }
+
+    private static string ImportFormatToStringConversion(ImportFormat importFormat)
+    {
+#pragma warning disable CS8524 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value.
+        return importFormat switch
+        {
+            ImportFormat.Cityvizor => "cityvizor",
+            ImportFormat.InternetStream => "internetstream",
+            ImportFormat.Ginis => "ginis",
+            ImportFormat.PboExpectedPlan => "pbo_expected_plan",
+            ImportFormat.PboRealPlan => "pbo_real_plan",
+            ImportFormat.PboAaNames => "pbo_aa_names"
+        } ;
+#pragma warning restore CS8524 // The switch expression does not handle some values of its input type (it is not exhaustive) involving an unnamed enum value.
+    }
+
+    private static ImportFormat StringToImportFormatConversion(string importFormatString) 
+    {
+        return importFormatString switch
+        {
+            "cityvizor" => ImportFormat.Cityvizor,
+            "internetstream" => ImportFormat.InternetStream,
+            "ginis" => ImportFormat.Ginis,
+            "pbo_expected_plan" => ImportFormat.PboExpectedPlan,
+            "pbo_real_plan" => ImportFormat.PboRealPlan,
+            "pbo_aa_names" => ImportFormat.PboAaNames,
+            _ => throw new ArgumentException($"Failed to convert string {importFormatString} to ImportFormat enum.")
+        };
     }
 }
