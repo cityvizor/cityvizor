1c4562d1ff7bde9d4bb2b11fb2defdd2605c49df
oroztocil <roztocil@outlook.com>
2024-06-01 23:07:13 +0200

    Feature/483 Bulk subprofile accounting import (#650)
    Add API for importing accounting data for subprofiles in a single ZIP, refactor API imports

--- server/src/routers/import/accounting.ts
+++ server/src/routers/import/accounting.ts
@@ -1,21 +1,19 @@
 import express from "express";
-
 import path from "path";
 import multer from "multer";
 import acl from "express-dynacl";
 import schema from "express-jsonschema";
-
 import extract from "extract-zip";
 import fs from "fs-extra";
-
 import config from "../../config";
-
 import { db } from "../../db";
 import { YearRecord, ProfileRecord } from "../../schema";
 import { ImportRecord } from "../../schema/database/import";
 import { move } from "fs-extra";
 import { DateTime } from "luxon";
 import { Import } from "../../worker/import/import";
+import { isPositiveInteger } from "../../utils";
+import { logger } from "../../logger";
 const router = express.Router();
 
 export const ImportAccountingRouter = router;
@@ -63,7 +61,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.PAYMENTS_FILE, false);
+    return processCsvImport(req, res, FileType.PAYMENTS_FILE, false);
   }
 );
 
@@ -73,7 +71,7 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.PAYMENTS_FILE, true);
+    return processCsvImport(req, res, FileType.PAYMENTS_FILE, true);
   }
 );
 
@@ -83,7 +81,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.EVENTS_FILE, false);
+    return processCsvImport(req, res, FileType.EVENTS_FILE, false);
   }
 );
 
@@ -93,7 +91,7 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.EVENTS_FILE, true);
+    return processCsvImport(req, res, FileType.EVENTS_FILE, true);
   }
 );
 
@@ -103,7 +101,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.DATA_FILE, false);
+    return processCsvImport(req, res, FileType.DATA_FILE, false);
   }
 );
 
@@ -113,7 +111,7 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.DATA_FILE, true);
+    return processCsvImport(req, res, FileType.DATA_FILE, true);
   }
 );
 
@@ -123,7 +121,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.EXPECTED_PLAN_FILE, false);
+    return processCsvImport(req, res, FileType.EXPECTED_PLAN_FILE, false);
   }
 );
 
@@ -133,7 +131,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.REAL_PLAN_FILE, false);
+    return processCsvImport(req, res, FileType.REAL_PLAN_FILE, false);
   }
 );
 
@@ -143,7 +141,7 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.EXPECTED_PLAN_FILE, true);
+    return processCsvImport(req, res, FileType.EXPECTED_PLAN_FILE, true);
   }
 );
 
@@ -153,7 +151,7 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.REAL_PLAN_FILE, true);
+    return processCsvImport(req, res, FileType.REAL_PLAN_FILE, true);
   }
 );
 router.post(
@@ -162,7 +160,7 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.AA_NAMES_FILE, false);
+    return processCsvImport(req, res, FileType.AA_NAMES_FILE, false);
   }
 );
 
@@ -172,94 +170,9 @@ router.patch(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
-    return createWorkerTask(req, res, FileType.AA_NAMES_FILE, true);
+    return processCsvImport(req, res, FileType.AA_NAMES_FILE, true);
   }
 );
-async function createWorkerTask(
-  req,
-  res,
-  fileType: FileType,
-  isAppend: boolean
-) {
-  // When file missing throw error immediately
-  if (!req.files || !req.files[fileType.toString()])
-    return res.status(400).send("Missing data file or zip file");
-  if (isNaN(req.body.year)) return res.status(400).send("Invalid year value");
-
-  // check if tokenCode in profile is same as in token. if not, the token has been revoked (revoke all current tokens by changing the code)
-  const profile = await db<ProfileRecord>("app.profiles")
-    .select("id", "tokenCode")
-    .where({ id: req.params.profile })
-    .first();
-  if (
-    !profile ||
-    (req.user.tokenCode && req.user.tokenCode !== profile.tokenCode)
-  )
-    return res.status(403).send("Token revoked.");
-
-  // check if imported year is created, if not create a new hidden year
-  let year: YearRecord | undefined = await db<YearRecord>("app.years")
-    .where({ profileId: req.params.profile, year: req.body.year })
-    .first();
-
-  if (!year) {
-    const yearInsert: YearRecord = await db<YearRecord>("app.years")
-      .insert(
-        {
-          profileId: Number(req.params.profile),
-          year: req.body.year,
-        },
-        ["profileId", "year"]
-      )
-      .then();
-    year = yearInsert ? yearInsert[0] : null;
-    if (!year)
-      return res
-        .status(500)
-        .send("Failed to create new accounting year in database.");
-  }
-
-  const importDir = await Import.createImportDir();
-
-  if (req.files[fileType.toString()] && req.files[fileType.toString()][0]) {
-    await move(
-      req.files[fileType.toString()][0].path,
-      path.join(importDir, `${fileType.toString()}.csv`)
-    );
-  }
-  // add import task to database queue (worker checks the table)
-  const importData: Partial<ImportRecord> = {
-    profileId: year.profileId,
-    year: year.year,
-
-    userId: req.user ? req.user.id : undefined,
-
-    created: DateTime.local().toJSDate(),
-
-    status: "pending",
-    error: undefined,
-
-    validity: req.body.validity || undefined,
-    format: importFormats[fileType],
-    append: isAppend,
-    importDir,
-  };
-
-  const result = await db<ImportRecord>("app.imports").insert(importData, [
-    "id",
-  ]);
-  const importId = result ? result[0].id : null;
-
-  if (!importId)
-    return res.status(500).send("Failed to create import record in database.");
-
-  // get the current full task info (including default values etc.) and return it to the client
-  const importDataFull = await db<ImportRecord>("app.imports")
-    .where({ id: importId })
-    .first();
-
-  res.json(importDataFull);
-}
 
 // Technical debt for historial reasons to not break backwards compatibility.
 // TODO: Properly refactor this one day. Keeping this code to enable upload via ZIP.
@@ -273,55 +186,33 @@ router.post(
   schema.validate(importAccountingSchema),
   acl("profile-accounting:write"),
   async (req, res) => {
+    logger.info(`Processing accounting import for profile '${req.params?.profile}' and year '${req.body?.year}'.`);
+
     const reqFiles = req.files as {
       [fieldname: string]: Express.Multer.File[];
     };
 
     // When both files missing throw error immediately
     if (!reqFiles || (!reqFiles.accounting && !reqFiles.zipFile)) {
-      return res.status(400).send("Missing data file or zip file");
+      return res.status(400).send("Missing data file or zip file.");
     }
-    if (isNaN(req.body.year)) {
-      return res.status(400).send("Invalid year value");
-    }
-
-    // check if tokenCode in profile is same as in token. if not, the token has been revoked (revoke all current tokens by changing the code)
-    const profile = await db<ProfileRecord>("app.profiles")
-      .select("id", "tokenCode")
-      .where("id", req.params.profile)
-      .first();
-
-    if (
-      !profile ||
-      (req.user.tokenCode && req.user.tokenCode !== profile.tokenCode)
-    )
-      return res.status(403).send("Token revoked.");
-
-    // check if imported year is created, if not create a new hidden year
-    let year: YearRecord | undefined = await db<YearRecord>("app.years")
-      .where("profileId", req.params.profile)
-      .andWhere("year", req.body.year)
-      .first();
-
-    if (!year) {
-      const yearInsert: YearRecord = await db<YearRecord>("app.years")
-        .insert(
-          {
-            profileId: Number(req.params.profile),
-            year: req.body.year,
-          },
-          ["profileId", "year"]
-        )
-        .then();
-      year = yearInsert ? yearInsert[0] : null;
-      if (!year)
-        return res
-          .status(500)
-          .send("Failed to create new accounting year in database.");
+    if (!isPositiveInteger(req.body.year)) {
+      return res.status(400).send("Invalid year value.");
     }
 
+    const profileId = Number(req.params.profile);
+    const year = Number(req.body.year);
+    const isAuthorized = await checkToken(profileId, req.user?.tokenCode);
+
+    if (!isAuthorized) {
+      return res.status(403).send("Invalid token.");
+    }
+
+    const yearRecord = await ensureYearExists(profileId, year);
     const importDir = await Import.createImportDir();
 
+    // If ZIP file has been upload it, extract it to the import directory
+    // Otherwise move the CSV file to the import directory
     if (reqFiles.zipFile && reqFiles.zipFile[0]) {
       await extractZip(reqFiles.zipFile[0].path, importDir);
     } else {
@@ -332,42 +223,227 @@ router.post(
         );
     }
 
-    // add import task to database queue (worker checks the table)
-    const importData: Partial<ImportRecord> = {
-      profileId: year.profileId,
-      year: year.year,
-
-      userId: req.user ? req.user.id : undefined,
-
-      created: DateTime.local().toJSDate(),
-
-      status: "pending",
-      error: undefined,
-
-      validity: req.body.validity || undefined,
-      format: "cityvizor",
-      append: false,
+    const importDataFull = await createImportJob(
+      "cityvizor",
       importDir,
-    };
-
-    const result = await db<ImportRecord>("app.imports").insert(importData, [
-      "id",
-    ]);
-    const importId = result ? result[0].id : null;
-
-    if (!importId)
-      return res
-        .status(500)
-        .send("Failed to create import record in database.");
-
-    // get the current full task info (including default values etc.) and return it to the client
-    const importDataFull = await db<ImportRecord>("app.imports")
-      .where({ id: importId })
-      .first();
+      yearRecord,
+      false,
+      req.body.validity,
+      req.user?.id
+    );
 
     return res.json(importDataFull);
   }
 );
+
+router.post(
+  "/profiles/:profile/subprofile-accounting",
+  upload.fields([{ name: "zipFile", maxCount: 1 }]),
+  schema.validate(importAccountingSchema),
+  acl("profile-accounting:write"),
+  async (req, res) => {
+    logger.info(`Processing subprofile accounting import for profile '${req.params?.profile}' and year '${req.body?.year}'.`);
+
+    const reqFiles = req.files as {
+      [fieldname: string]: Express.Multer.File[];
+    };
+
+    if (!reqFiles || !reqFiles.zipFile) {
+      return res.status(400).send("Missing zip file.");
+    }
+
+    if (!isPositiveInteger(req.body.year)) {
+      return res.status(400).send("Invalid year value.");
+    }
+
+    const profileId = Number(req.params.profile);
+    const year = Number(req.body.year);
+    const isAuthorized = await checkToken(profileId, req.user?.tokenCode);
+
+    if (!isAuthorized) {
+      return res.status(403).send("Invalid token.");
+    }
+
+    // Create shared directory and unzip main ZIP file
+    const sharedDir = await Import.createImportDir();
+    await extractZip(reqFiles.zipFile[0].path, sharedDir);
+    const subprofileDir = path.join(sharedDir, fs.readdirSync(sharedDir)[0]);
+
+    // Get all subprofile ZIP file paths and associated ICO values
+    const subprofileImports: [path: string, ico: string][] = fs
+      .readdirSync(subprofileDir)
+      .filter(fileName => path.extname(fileName) === ".zip")
+      .map(fileName => {
+        // Identify profile from ICO
+        const ico = fileName.split("_")[1].slice(2);
+        if (!ico.match(/^\d{8}$/)) {
+          throw new Error(`Invalid subprofile ZIP file name '${fileName}'`);
+        }
+
+        return [path.join(subprofileDir, fileName), ico];
+      });
+
+    logger.info(`Created directory '${sharedDir}' for subprofile import. ZIP files to be imported found: ${subprofileImports.length}.`);
+
+    for (const [filePath, ico] of subprofileImports) {
+      // Find profile with the provided ICO
+      // Beware! If there are multiple profiles with the same ICO, the first is selected and the rest is ignored
+      const subprofileId = await db<ProfileRecord>("app.profiles")
+        .select("id")
+        .where({ ico: ico })
+        .first()
+        .then(result => result?.id);
+
+      if (!subprofileId) {
+        logger.warn(`No profile exists for subprofile import with ICO '${ico}'.`);
+        continue;
+      }
+
+      const yearRecord = await ensureYearExists(subprofileId, year);
+      const importDir = await Import.createImportDir();
+
+      // Unzip subprofile ZIP file to subprofile import directory
+      await extractZip(filePath, importDir);
+
+      await createImportJob(
+        "cityvizor",
+        importDir,
+        yearRecord,
+        false,
+        req.body.validity,
+        req.user?.id
+      );
+    }
+
+    return res.sendStatus(200);
+  }
+);
+
+async function processCsvImport(
+  req,
+  res,
+  fileType: FileType,
+  isAppend: boolean
+) {
+  logger.info(`Processing CSV import for profile '${req.params?.profile}' and year '${req.body?.year}'.`);
+
+  // When file missing throw error immediately
+  if (!req.files || !req.files[fileType.toString()]) {
+    return res.status(400).send("Missing data file or zip file");
+  }
+
+  if (!isPositiveInteger(req.body.year)) {
+    return res.status(400).send("Invalid year value.");
+  }
+
+  const profileId = Number(req.params.profile);
+  const year = Number(req.body.year);
+  const isAuthorized = await checkToken(profileId, req.user?.tokenCode);
+
+  if (!isAuthorized) {
+    return res.status(403).send("Invalid token.");
+  }
+
+  const yearRecord = await ensureYearExists(profileId, year);
+  const importDir = await Import.createImportDir();
+
+  // Move the CSV file to the import directory
+  if (req.files[fileType.toString()] && req.files[fileType.toString()][0]) {
+    await move(
+      req.files[fileType.toString()][0].path,
+      path.join(importDir, `${fileType.toString()}.csv`)
+    );
+  }
+
+  const importDataFull = await createImportJob(
+    importFormats[fileType],
+    importDir,
+    yearRecord,
+    isAppend,
+    req.body.validity,
+    req.user?.id
+  );
+
+  res.json(importDataFull);
+}
+
+async function checkToken(
+  profileId: number,
+  tokenCode?: number
+): Promise<boolean> {
+  if (!tokenCode) {
+    return false;
+  }
+
+  const profile = await db<ProfileRecord>("app.profiles")
+    .select("id", "tokenCode")
+    .where("id", profileId)
+    .first();
+
+  return profile != undefined && tokenCode === profile.tokenCode;
+}
+
+async function ensureYearExists(
+  profileId: number,
+  yearParam: number
+): Promise<YearRecord> {
+  let year = await db<YearRecord>("app.years")
+    .where({ profileId: profileId, year: yearParam })
+    .first();
+
+  year ??= await db<YearRecord>("app.years")
+    .insert(
+      {
+        profileId: profileId,
+        year: yearParam,
+      },
+      ["profileId", "year", "validity"]
+    )
+    .then(result => result[0]);
+
+  if (!year) {
+    throw new Error("Failed to create accounting year in database.");
+  }
+
+  return year;
+}
+
+async function createImportJob(
+  format: Import.Format,
+  importDir: string,
+  year: YearRecord,
+  isAppend: boolean,
+  validity?: string,
+  userId?: number
+): Promise<ImportRecord> {
+  // Add import task to database queue (worker checks the table)
+  const importData: Partial<ImportRecord> = {
+    profileId: year.profileId,
+    year: year.year,
+    userId: userId,
+    created: DateTime.local().toJSDate(),
+    status: "pending",
+    error: undefined,
+    validity: validity,
+    format: format,
+    append: isAppend,
+    importDir,
+  };
+
+  const importId = await db<ImportRecord>("app.imports")
+    .insert(importData, ["id"])
+    .then(result => result[0]?.id);
+
+  const importRecord = await db<ImportRecord>("app.imports")
+    .where({ id: importId })
+    .first();
+
+  if (!importRecord) {
+    throw new Error("Failed to create import record in database.");
+  }
+
+  return importRecord;
+}
 
 async function extractZip(zipFile: string, unzipDir: string) {
   try {
